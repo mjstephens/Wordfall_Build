5265458e6cda216a84235e61a07892cd703e666f
Michael Stephens <michaelstephens@Michaels-MBP-16.localdomain>
2024-08-19 22:04:23 -0400

    first build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Galaxy Gourd-Wordfall-1.0";
+const contentToCache = [
+    "Build/8-19-24_01.loader.js",
+    "Build/8-19-24_01.framework.js",
+    "Build/8-19-24_01.data",
+    "Build/8-19-24_01.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
